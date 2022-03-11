30077976f51a513badb52e055053101eb0ff403a
Nikolas Schildberg <nikolas.schildberg@gmail.com>
2022-03-10 21:06:46 -0300

    Finished Lists tutorial

--- list_functions_and_methods.py
+++ list_functions_and_methods.py
@@ -1,4 +1,7 @@
 # Creating data
+import this
+
+
 numbers = [1, 2, 3, 4, 5, 6]
 cities = ["Taubaté", "Ubatuba", "São Paulo", "Ilhabela"]
 
@@ -13,3 +16,17 @@ print("max(numbers):", max(numbers))
 cities.append("Rio de Janeiro")
 print("Now:", cities)
 print("Popped:", cities.pop(2), cities)
+
+
+def find_city(this_city):
+    if(this_city in cities):
+        print(this_city, " found at cities[",
+              cities.index(this_city), "]", sep="")
+    else:
+        print("City", this_city, "not found in cities.")
+
+
+this_city = "Ilhabela"
+find_city(this_city)
+this_city = "São Paulo"
+find_city(this_city)
